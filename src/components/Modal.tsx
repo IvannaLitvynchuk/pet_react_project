c9d2336223d27efc79d404e034658302fff03a0e
Іванна Літвинчук <Yana@MacBook-Air-Ivanna.local>
2022-12-25 21:25:07 +0200

    created modal window

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import react from "react"
 
 interface ModalProps {
     children: React.ReactNode
@@ -8,7 +9,7 @@ interface ModalProps {
 export function Modal({children, title, onClose}: ModalProps) {
     return (
         <>
-            <div className="modal-bg"></div>
+            <div className="modal-bg" onClick={onClose}></div>
             <div className="modal-content">
                 <h1 className="modal-title">{ title }</h1>
                 { children }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ModalContext } from "../context/ModalContext";
 import { IProduct } from "../models";
+import { Modal } from "./Modal";
 
 interface ProductProps {
     product: IProduct
@@ -7,22 +9,37 @@ interface ProductProps {
 
 export function Product({ product }: ProductProps) {
     const [details, setDetails] = useState(false);
+    const {modal, open, close} = useContext(ModalContext)
+    const onOpenModal = () => {
+        open()
+        setDetails(prep => !prep)
+        console.log(product.id)
+    }
 
 
     return (
-        <div className="product-item">
+        <>
+            <div className="product-item">
             <img className="product-img" src={product.image} />
             <p className="product-title">{product.title}</p>
             <p className="product-price">{product.price}$</p>
-            <button className="product-btn" onClick={() => setDetails(prev => !prev)}>
-                {details ? "Hide details" : "Show details"}
+            <button className="product-btn" onClick={onOpenModal}>
+                Show details
             </button>
-            {details ?
-                <p className="product-description">{product.description}</p>
-            :
-                ""
-            }
-            
         </div>
+        { details ?
+        <>
+            {modal &&
+                <Modal title={product.title} onClose={close}>
+                    <p>{product.description}</p>
+                </Modal>
+            }
+        </>
+            :
+            ""
+        }
+        
+        </>
+        
     )
 }
